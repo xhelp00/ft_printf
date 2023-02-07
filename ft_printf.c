791630bdf75009b0358d79888706d143e87d16da
Petr Helebrant <xhelp00@gmail.com>
2023-02-07 17:58:13 +0100

    pseudo code

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,21 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 18:09:27 by phelebra          #+#    #+#             */
-/*   Updated: 2023/02/07 12:40:09 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/02/07 17:57:25 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+//the function checking parameters
+	// if/else for each case csdiupxX%
+	// each case will call its own function handling particular type
+
 int	ft_printf(const char *s, ...)
 {
 	va_list	args;
+
+
+	//loop through args
+	//call the function checking parameters
 }
