a74f8544c9dcb07c80660d859490568aa96432be
xhelp00 <xhelp00@gmail.com>
2023-02-08 15:19:21 +0100

    added functions paramtype and putstr

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 18:09:27 by phelebra          #+#    #+#             */
-/*   Updated: 2023/02/08 14:28:54 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/02/08 15:18:22 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,54 @@
 	// if/else for each case csdiupxX%
 	// each case will call its own function handling particular type
 
+int	ft_paramtype(va_list args, char c)
+{
+	int	size;
+
+	size = 0;
+	if (c == 'c')
+		size = 1;
+	else if (c == 's')
+		size = 1;
+	else if (c == 'p')
+		size =1;
+	else if (c == 'i' || c == 'd')
+		size =1;
+	else if (c == 'u')
+		size =1;
+	else if (c == 'x' || c == 'X')
+		size =1;
+	else if (c == '%')
+		size = ft_putchar('%');
+	return (size);
+}
+
 int	ft_printf(const char *s, ...)
 {
 	va_list	args;
-	int 	size;
+	int		size;
+	int		i;
 
-
+	i = 0;
+	size = 0;
+	va_start (args, s);
+	while (s[i] != '\0')
+	{
+		if (s[i] == '%')
+		{
+			size += ft_paramtype(args, s[i + 1]);
+			i++;
+		}
+		else
+		{
+			ft_putchar_fd(s[i], 1);
+			size++;
+			i++;
+		}
+	}
+	va_end(args);
 	//loop through args
 	//call the function checking parameters
-
 	//upon successful return, these functions return the number o characters printed
-	
 	return (size);
 }
